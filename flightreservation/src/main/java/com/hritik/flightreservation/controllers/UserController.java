4a882c55d945de2927267907b8fa244f514b6441
Hritik3108 <hritiksn009@gmail.com>
2023-05-22 20:19:33 +0530

    Registration Page jsp ready.

--- flightreservation/src/main/java/com/hritik/flightreservation/controllers/UserController.java
+++ flightreservation/src/main/java/com/hritik/flightreservation/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.hritik.flightreservation.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-
+	
+	@RequestMapping("/showReg")
+	public String showRegistration() {
+		return "login/registerUser";
+	}
 }
